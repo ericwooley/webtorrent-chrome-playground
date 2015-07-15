1fe1d6f98475d38e42c2cd76d39faf657906919b
Eric Wooley <Eric.Wooley@rosetta.com>
2015-07-14 22:00:03 -0700

    added torrent file writing

--- src/js/src/foreground.js
+++ src/js/src/foreground.js
@@ -1,22 +1,50 @@
 /* globals chrome */
 import dragDrop from 'drag-drop/buffer';
 
+const $$ = {
+  saveButton: document.querySelector('.save-button')
+};
+
+function writeFile(torrentFile, fileWriter, index = 0) {
+  fileWriter.onwriteend = () => {
+    console.log('Write completed.');
+  };
+  fileWriter.onerror = (e) => {
+    console.log('Write failed: ' + e.toString());
+  };
+  fileWriter.onprogress = (event) => {
+    const loadPercent = event.loaded / event.total * 100.0;
+    console.log('progress', loadPercent);
+  };
+
+  const buffer = torrentFile.getBuffer((err, buffer) => {
+    if(err) {
+      throw err;
+    }
+    console.log('writing buffer', torrentFile, buffer);
+    // const blob = new Blob(buffer, {type: torrentFile.type});
+    const blob = new Blob(buffer.toArrayBuffer(), {type: 'image/png'} );
+    fileWriter.write(blob);
+    console.log('wrote to disk');
+  });
+
+
+  // writeFile(torrentFile, fileWriter, index + 1);
+}
+function saveTorrentFile(file) {
+  return () => {
+    chrome.fileSystem.chooseEntry({type: 'saveFile'}, (fileEntry) => {
+      fileEntry.createWriter((fileWriter) => writeFile(file, fileWriter, 0));
+    });
+  };
+}
 function main(bg) {
-  // const client = bg;
-  let lastTorrent;
   dragDrop('#dragdrop', (files) => {
     console.log('adding files - test', files);
     bg.client.seed(files, (torrent) => {
-      lastTorrent = torrent;
       console.log('added files', torrent.files[0], torrent.infoHash);
-    });
-  });
-  const $$ = {
-    saveButton: document.querySelector('.save-button')
-  };
-  $$.saveButton.addEventListener('click', () => {
-    chrome.fileSystem.chooseFile({type: 'saveFile'}, (entry) => {
-      debugger;
+      $$.saveButton.removeEventListener('click');
+      $$.saveButton.addEventListener('click', saveTorrentFile(torrent.files[0]));
     });
   });
 }
